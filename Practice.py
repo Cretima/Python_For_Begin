f4b3cf18984f1c8edad858903b7138dc34658cc2
오세현(Oh SeHyun) <Pg.Cretima@gmail.com>
2022-02-06 00:35:02 +0900

    [Add] 파일 입출력 설명추가 및 여러 사례

--- Practice.py
+++ Practice.py
@@ -26,7 +26,21 @@ score_file =  open("score.txt", "r", encoding="utf-8")
 print(score_file.readline()) # 한줄만 읽기, 한 줄 읽고 커서는 다음 줄로 이동해 즉, 파일에 4줄이 있으면 출력문 4개를 입력해야해
 print(score_file.readline(), end="") # 위와 다르게 다음줄이 아닌 이어서 출력이 되
 score_file.close()
+# 파일읽기1 - 파일에 몇줄이 입력 되어있는지 모를때
+score_file =  open("score.txt", "r", encoding="utf-8")
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    print(line)
+score_file.close()
+# 파일 읽기2 - 파일의 모든 줄을 리스트에 저장해
+score_file =  open("score.txt", "r", encoding="utf-8")
+lines = score_file.readlines() # line 과 다르게 liens는 리스트 형식으로 저장!
+for line in lines:
+    print(line, end="")
 
+score_file.close()
 
 # 끄적끄적
 '''
